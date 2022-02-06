4e60bde73e471df20ebe60a7c2f49d73eb33eb8d
Andrija Gradečak <60037415+proto-forma@users.noreply.github.com>
2022-02-06 15:08:49 +0100

    Implementacija prijave i registracije

--- WebProject/WebProject/settings.py
+++ WebProject/WebProject/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# preusmjeravanje na homepage pri uspješnoj prijavi
+LOGIN_REDIRECT_URL = '/'
--- WebProject/main/views.py
+++ WebProject/main/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def homepage(request):
-    kod = '<h1>Tviter</h1>'
-    return HttpResponse(kod)
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+
+    return render(request, 'registration/register.html', context)
